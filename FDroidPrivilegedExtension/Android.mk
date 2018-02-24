cc62b6a5b0127e8359ff9ad1d42b6ce1524a559c
Tobias Tefke <tobias.tefke@gmail.com>
2018-02-24 17:54:42 +0100

    Add FDroidPrivilegedExtensions permission file
    Change-Id: Ic634d302c8186d0fa76d1846482f129697d84d13

--- FDroidPrivilegedExtension/Android.mk
+++ FDroidPrivilegedExtension/Android.mk
@@ -8,3 +8,11 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := permissions_org.fdroid.fdroid.privileged.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := permissions_org.fdroid.fdroid.privileged.xml
+include $(BUILD_PREBUILT)
