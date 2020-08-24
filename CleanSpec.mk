195312d069252f4764f9477296b7e11a0285b9d8
Tobias Tefke <tobias@tefke.name>
2020-08-24 18:10:30 +0200

    Add CleanSpec.mk
    Change-Id: I0d56e1c0bf092140c31cd4d5833d3d8d3182929f

---
+++ CleanSpec.mk
@@ -0,0 +1,36 @@
+##########################
+# FDroid and microG
+##########################
+# F-Droid additional repos
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/org.fdroid.fdroid/additional_repos.xml)
+# Microg maps
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/permissions//com.google.android.maps.xml)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/framework/com.google.android.maps.jar)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/addon.d/10-mapsapi.sh)
+# DroidGuard
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DroidGuard/DroidGuard.apk)
+# FakeStore
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/permissions/privapp-permissions-com.android.vending.xml)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/FakeStore/FakeStore.apk)
+# FDroid
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/FDroid/FDroid.apk)
+# FDroid privileged extension
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/permissions/privapp-permissions-org.fdroid.fdroid.privileged.xml)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/addon.d/80-fdroid.sh)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/FDroidPrivilegedExtension/FDroidPrivilegedExtension.apk)
+# GmsCore
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/permissions/privapp-permissions-com.google.android.gms.xml)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/GmsCore/GmsCore.apk)
+# GsfProxy
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/GsfProxy/GsfProxy.apk)
+# UnifiedNlp and location providers
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/NetworkLocation/NetworkLocation.apk)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/MozillaNlpBackend/MozillaNlpBackend.apk)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/NominatimNlpBackend/NominatimNlpBackend.apk)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/OpenBmapNlpBackend/OpenBmapNlpBackend.apk)
+
+
+##########################
+# EMail
+##########################
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/product/app/Email/Email.apk)
